309bb7f6488abc1a07490df8693a934daaee1ec1
Ben Matthies <ben@matthsch.de>
2026-01-17 13:17:18 +0100

    Finally get media keys to work on i3

--- modules/services/window-manager/i3.nix
+++ modules/services/window-manager/i3.nix
@@ -18,6 +18,9 @@ in {
     };
 
     home-manager.users.ben = { pkgs, ... }: {
+      # Media keys.
+      services.playerctld.enable = true;
+
       xsession.windowManager.i3 = {
         enable = true;
         config = {
@@ -27,6 +30,12 @@ in {
             command = "i3bar";
             statusCommand = "${pkgs.i3status}/bin/i3status";
           } ];
+          keybindings = lib.mkOptionDefault {
+            XF86AudioPlay = "exec playerctl play-pause";
+            XF86AudioPause = "exec playerctl play-pause";
+            XF86AudioPrev = "exec playerctl previous";
+            XF86AudioNext = "exec playerctl next";
+          };
           gaps = {
             inner = 8;
             outer = 4;
